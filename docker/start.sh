755c64067efdd45c9289acf3a7473e5d1a134769
Emriqurrizal Yahya Nurramdahan <emriqurrizal@gmail.com>
2026-06-07 17:35:49 +0700

    dockerfile start.sh logging

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+sed -i 's/;catch_workers_output = yes/catch_workers_output = yes/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i 's/;decorate_workers_output = no/decorate_workers_output = no/g' /usr/local/etc/php-fpm.d/www.conf
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
