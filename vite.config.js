91ffa497382f94700e4953f695a8ed4247dfbf89
A225N <allan222005@gmail.com>
2026-06-16 16:57:06 +0700

    add: istanbul vite plugin instrument

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import istanbul from 'vite-plugin-istanbul';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+
+    istanbul({
+      include: 'resources/js/*',
+      exclude: ['node_modules', 'tests/'],
+      extension: ['.js', '.ts', '.vue', '.jsx', '.tsx'],
+      requireEnv: false,
+      forceBuildInstrument: mode === 'test',
+    }),
+  ],
+}));
