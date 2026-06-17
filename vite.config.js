6372037c1a71435db706c261aa78d7614d9dc798
A225N <allan222005@gmail.com>
2026-06-17 14:53:38 +0700

    fix: more fixing

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import istanbul from 'vite-plugin-istanbul';
 
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    tailwindcss(),
+export default defineConfig(({ mode }) => {
+  const enableCoverage = process.env.NODE_ENV === 'test';
 
-    istanbul({
-        include: 'resources/js/**/*.{js,ts,vue}',
-        exclude: ['node_modules', 'tests/**', '**/*.spec.ts', '**/*.test.ts'],
-        extension: ['.js', '.ts', '.vue', '.jsx', '.tsx'],
-        requireEnv: true,
-        checkProd: false,
-    })
-  ],
-}));
+  return {
+    plugins: [
+      laravel({
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
+      }),
+      tailwindcss(),
+
+      enableCoverage && istanbul({
+          include: 'resources/js/**/*.{js,ts,vue}',
+          exclude: ['node_modules', 'tests/**', '**/*.spec.ts', '**/*.test.ts'],
+          extension: ['.js', '.ts', '.vue', '.jsx', '.tsx'],
+          requireEnv: false,
+          checkProd: false,
+      })
+    ].filter(Boolean),
+  }
+});
